63c945c528ee28e8269eba1382eb4d4120c2eb15
Yogesh Pisal <153059079+Yogeshpisal216@users.noreply.github.com>
2024-02-18 11:36:04 +0530

    Update stdent.groovy

--- stdent.groovy
+++ stdent.groovy
@@ -6,6 +6,7 @@ pipeline {
         stage('git commit') {
             steps {
                 echo 'welcome to pipeline'
+                git 'https://github.com/Yogeshpisal216/yogi.git'
                   }
              }
         stage('maven package'){
